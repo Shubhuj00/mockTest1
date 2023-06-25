f05e5e0fb18752a6faf5d31454476fcd8a086931
Shubhuj Shukla <shubhuj322@gmail.com>
2023-06-25 13:04:49 +0530

    First Question

---
+++ MoveZero.java
@@ -0,0 +1,25 @@
+
+class MoveZero{
+    public static void moveZero(int[] nums, int n){
+        int count=0;
+        for(int i =0; i<n; i++){
+            if(nums[i] != 0){
+                nums[count++]=nums[i];
+                
+            }
+        }
+        while(count < n){
+            nums[count++]=0;
+            
+        }
+    }
+    public static void main(String[] args){
+        int nums[]={0,1,0,3,12};
+        int n=nums.length;
+        moveZero(nums,n);
+        System.out.println("Array after adjusting 0 :");
+        for(int i=0; i<n; i++){
+            System.out.print(nums[i]+" ");
+        }
+    }
+}
